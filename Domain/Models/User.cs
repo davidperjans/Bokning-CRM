af9bfcc2580eb3c4272634d5c05b607a9b7cf100
davidperjans <perjansdavid@gmail.com>
2026-01-14 18:57:44 +0100

    init db

---
+++ Domain/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+    }
+}
---
+++ Infrastructure/Database/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // Define DbSets for your entities here
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Configure your entity mappings here
+        }
+    }
+}
