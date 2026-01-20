9eb0baccd4722bca285a363fbccd27c5db10f1ba
Mikael Daskalou <mikael.daskalou@hotmail.com>
2026-01-20 12:18:46 +0100

    implementering av Login och Jwt TokenService

---
+++ API/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using Application.Users.Commands.LoginUser;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        
+        public AuthController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        
+        
+        
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginCommand command)
+        {
+            var result = await _mediator.Send(command);
+
+            if (!result.IsSuccess)
+            {
+                return Unauthorized(new { message = result.ErrorMessage });
+            }
+
+            return Ok(result.Value);
+        }
+    }
+}
---
+++ Application/Interface/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Models;
+
+namespace Application.Interface
+{
+    public interface ITokenService
+    {
+        string GenerateJwtToken(User user);
+    }
+}
